50fa703654173796b5dfa6153a9dc7e6780ce574
tbec <tbecnel14@gmail.com>
2019-09-13 11:48:22 -0600

    .

--- main/component.mk
+++ main/component.mk
@@ -1,2 +1,2 @@
- COMPONENT_EMBED_TXTFILES := ${PROJECT_PATH}/cert/ca_airu.pem 
+ COMPONENT_EMBED_TXTFILES := ${PROJECT_PATH}/cert/ca_tetrad.pem 
 COMPONENT_EMBED_FILES := jquery.gz code.js index.html style.css
